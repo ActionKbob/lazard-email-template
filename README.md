6429adb69eeeac99424af16319930065d91dcb7e
Anthony Capobianchi <anthony.capobianchi@gmail.com>
2024-10-18 15:04:34 -0400

    Added minification to the css before it gets inlined to the dist

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [Reference](https://www.figma.com/design/DQOzjDZf7trxfL4D9jg3Ox/New-Email-Library?t=2ABFyPpxE1ZTgt7E-0)
 
 #### General Changes:
-- Left and Right borders for the modules changed from 0.5px to 1px, Outlook does not support border sizes less than 1
+- Unordered List bullets do not have their color changed to Sapphire in some clients due to limited support for the ::marker pseudo selector as well as list-style: none not being applied in Outlook
 
 #### Color changes made due to lack of opacity support in email clients:
 - Changed module frame border color from #171A1C to #dadada on "Parchment", "Sage", and "Clarity" variants
--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ import path from 'path'
 
 import file_include from 'gulp-file-include';
 import juice from 'juice';
+import cleanCss from 'gulp-clean-css';
 
 import { deleteAsync } from 'del';
 
@@ -29,6 +30,14 @@ function templateInjectionTask( cb )
 			imgBase: "https://info.lazardassetmanagement.com/rs/394-MLC-997/images"
 		}
 	}) )
+	.pipe( gulp.dest( tempDir ) )
+	.on( 'end', cb )
+}
+
+function minifyCss( cb )
+{
+	gulp.src( [ 'src/styles/index.css' ] )
+	.pipe( cleanCss() )
 	.pipe( gulp.dest( tempDir ) )
 	.on( 'end', cb )
 }
@@ -54,7 +63,8 @@ function inlineCssTask( cb )
 			preserveMediaQueries,
 			webResources: {
 				images: 0
-			}
+			},
+			removeStyleTags: false
 		}, function( err, outputHtml ) {
 			if( !fs.existsSync( dir ) )
 				fs.mkdirSync( dir, { recursive : true } );
@@ -71,11 +81,11 @@ async function cleanup( cb )
 	cb();
 }
 
-function watchTask(  )
+function watchTask()
 {
 	gulp.watch( [ './src/**/*.html', './src/**/*.css' ], gulp.series( 'build' ) );
 }
 
-gulp.task( 'build', gulp.series( cleanup, templateInjectionTask, inlineCssTask ) );
+gulp.task( 'build', gulp.series( cleanup, templateInjectionTask, minifyCss, inlineCssTask ) );
 gulp.task( 'watch', gulp.series( 'build', watchTask ) );
 gulp.task( 'clean', gulp.series( cleanup ) );
